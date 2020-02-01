6d3c7126d2eb133f8b6b3c5b57d4ccd9eb0ff97c
dowripple <53706893+dowripple@users.noreply.github.com>
2020-02-01 08:39:26 -0600

    progress! regular part done, started bonus

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,9 @@ d3.json('../../samples.json').then((importedData) => {
 
     var data = importedData;
 
+    // get a look at the object
+    console.log(data);
+    
     // get unique list of test ids
     var test_ids = data.names
 
@@ -17,59 +20,113 @@ d3.json('../../samples.json').then((importedData) => {
 
     }
 
+    // function that will refresh each component based on the drop down value
+    function optionChanged(test_id) {
+
+        var data = importedData;
+
+        // filter the data objects
+        // sample info first
+        var filteredSampleData = data.samples.filter(sample => sample.id === test_id );
+
+        // doing the demographic data here, to be used later (for some reason i had to parseint)
+        var filteredMetaData = data.metadata.filter(meta => parseInt(meta.id) === parseInt(test_id) );
+
+        // pulling out the arrays we need using map
+        // sample values
+        var sample_values = filteredSampleData.map(row => row.sample_values);
+
+        // slice the first rows from the sample_values object (array at )
+            // thankfully the data is sorted descending order
+        sample_values = sample_values[0].slice(0,10).reverse();
+
+        // otu_ids
+        var otu_ids = filteredSampleData.map(row => row.otu_ids);
+
+        // grab the first 10
+        otu_ids = otu_ids[0].slice(0,10).reverse();
+
+        // aapend the text to the beginning of the id, but in a new array
+        var otu_id_labels = [];
+        for (i = 0; i < otu_ids.length; i++) {
+
+            otu_id_labels.push('OTU '.concat(otu_ids[i]));
+
+        }
+
+        // otu_labels
+        var otu_labels = filteredSampleData.map(row => row.otu_labels);
+
+        // grab the first 10
+        otu_labels = otu_labels[0].slice(0,10).reverse();
+
+        // // setup the trace
+        var trace1 = {
+            x: sample_values,
+            y: otu_id_labels,
+            type: 'bar',
+            orientation: 'h',
+            // width: 500,
+            text: otu_labels
+        }
+
+        // data for bar chart
+        var data = [trace1]
+
+        // layout
+        var layout = {
+            title: 'Top 10 Samples'  
+        }
+
+        // plot chart
+        Plotly.newPlot("bar", data, layout);
+
+        // bubble chartm (we already have the series)
+        var traceB = {
+            x: otu_ids,
+            y: sample_values,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            },
+            text: otu_labels
+
+        }
+
+        var data = [traceB];
+
+        var layout = {
+            title: 'Bubble Stuff'
+        }
+
+        // plot bubble chart on bubble div
+        Plotly.newPlot('bubble',data,layout)
+
+        // demographis data
+        var panelDiv = d3.select('#sample-metadata');
+
+        // add a new div
+        var demoTable = panelDiv.append('table').classed('table table-striped', true);
+
+        // var demoTbody = demoTable.append('tbody')
+        var row = demoTable.append('tr').html(`<td>id:</td><td>${filteredMetaData[0].id}</td>`);       
+        var row = demoTable.append('tr').html(`<td>ethnicity:</td><td>${filteredMetaData[0].ethnicity}</td>`);
+        var row = demoTable.append('tr').html(`<td>gender:</td><td>${filteredMetaData[0].gender}</td>`);    
+        var row = demoTable.append('tr').html(`<td>age:</td><td>${filteredMetaData[0].age}</td>`);    
+        var row = demoTable.append('tr').html(`<td>location:</td><td>${filteredMetaData[0].location}</td>`);
+        var row = demoTable.append('tr').html(`<td>bbtype:</td><td>${filteredMetaData[0].bbtype}</td>`);     
+        var row = demoTable.append('tr').html(`<td>wfreq:</td><td>${filteredMetaData[0].wfreq}</td>`);
+
+    }
+
+    //selDataset.on('change', optionChanged())
     // for now just get the value from the select box
     var test_id = selDataset.node().value;
 
-    // filter the data object
-    var filteredData = data.samples.filter(sample => sample.id === test_id )
-
-    // console.log(filteredData)
-
-    // pulling out the arrays we need using map
-    // sample values
-    var sample_values = filteredData.map(row => row.sample_values) //.splice(0, 10);
-
-    // slice the first rows from the sample_values object (array at )
-        // thankfully the data is sorted descending order
-    sample_values = sample_values[0].slice(0,10);
-
-    // otu_ids
-    var otu_ids = filteredData.map(row => row.otu_ids);
-
-    // grab the first 10
-    otu_ids = otu_ids[0].slice(0,10);
-
-    console.log(otu_ids);
-
-    // otu_labels
-    var otu_labels = filteredData.map(row => row.otu_labels);
-
-    // grab the first 10
-    otu_labels = otu_labels[0].slice(0,10);
-
-   
-    
-    // // setup the trace
-    var trace1 = {
-        x: sample_values,
-        y: otu_ids,
-        type: 'bar',
-        orientation: 'h',
-        width: 500,
-        text: otu_labels
-    }
-
-    var data = [trace1]
-
-    var layout = {
-      title: 'Top 10 Samples'  
-    }
-
-    Plotly.newPlot("bar", data, layout);
-
-    // check values
-    // console.log(sample_values);
-
-
+    // initialize based on the current value in drop down
+    optionChanged(test_id);
 
 });
+
+
